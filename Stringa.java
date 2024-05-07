0c2447974a59d796105722a0744871ba27915419
alfo0924 <146694784+alfo0924@users.noreply.github.com>
2024-05-07 11:50:37 +0800

    Update Stringa.java

--- Stringa.java
+++ Stringa.java
@@ -21,16 +21,19 @@ public class Stringa
 
             Scanner scanner=new Scanner(System.in);
 
-            // String acc="cccc8888";
-            // String pass="cccc7777";
+    
             System.out.println("Input 6 charaters : ");
             String type=scanner.nextLine();
 
 
-            //第一題
+            //第一題  密碼(字元)轉換 a換4,s換5,t換7,o換0
+
+
+
+            //建立條件判斷式 若字串大小大於等於6 
            if(type.length()>=6)
            {
-    
+                    //寫條件方法 記得加= 才可以更新數值
                     type=type.replace("a","4" );
                     type=type.replace("A","4" );
            
@@ -50,19 +53,22 @@ public class Stringa
 
                   }
 
-
+                    //印出結果
                 System.out.println(type);
 
 
  
 
-                //第二題
+                //第二題 取得字串內容 以e開頭f結尾 再把字串內容印出
 
+
+                  //輸入6個字串字元
                   System.out.println("Input 6 charaters again :");
                   String type2=scanner.nextLine();
                   if(type2.length()>6)
                   {
 
+                    //找出e的索引位置和f的索引位置再置入subString函式 取索引值.subString(開始,結尾)
                     type2=type2.substring(type2.indexOf('e'),type2.indexOf('f') );
 
         
@@ -72,8 +78,13 @@ public class Stringa
                         System.out.println("Charaters doesn't exist !");
                     }
     
+                        //印出最後結果
                         System.out.println(type2);
 
+
+
+
+                        
   
             }         
 
